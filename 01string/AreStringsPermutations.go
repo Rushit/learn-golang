e013a19fe935a00afde25c665f7b4ca1def2bae4
Rushit <rpatel@noticewala.com>
2016-07-01 00:14:18 -0700

    rename chapter

---
+++ 01string/AreStringsPermutations.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"sort"
+	"strings"
+)
+
+func main() {
+	println(AreStringPermutation("cat", "act"))
+	println(AreStringPermutation("cat", "actt"))
+
+}
+
+func AreStringPermutation(str1 string, str2 string) bool {
+	if (len(str1) != len(str2)) {
+		return false
+	};
+	sorted1 := SortString(str1)
+	sorted2 := SortString(str2)
+
+	return strings.EqualFold(sorted1, sorted2);
+}
+func SortString(w string) string {
+	s := strings.Split(w, "")
+	sort.Strings(s)
+	return strings.Join(s, "")
+}
---
+++ 01string/RotateMatrix.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	matrix := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+	PrintMatrix(matrix)
+	RotateMatrix(matrix, 3)
+	fmt.Println("Rotated matrix:")
+	PrintMatrix(matrix)
+
+}
+
+func RotateMatrix(matrix [][]int, row int) {
+	for layer := 0; layer < row / 2; layer++ {
+		first := layer
+		last := row - 1 - layer
+		for i := first; i < last; i++ {
+			offset := i - first
+
+			top := matrix[first][i]
+
+			matrix[first][i] = matrix[last - offset][first]
+			matrix[last - offset][first] = matrix[last][last - offset]
+			matrix[last][last - offset] = matrix[i][last]
+			matrix[i][last] = top
+		}
+	}
+}
+
+func PrintMatrix(matrix [][]int) {
+	for i := range matrix {
+		fmt.Println(matrix[i])
+	}
+}
