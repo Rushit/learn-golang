f4aa110d2836e4e603d5703ca32cac82daea1bae
Rushit <rpatel@noticewala.com>
2016-06-30 21:13:14 -0700

    first problem

---
+++ string/IsUniqueCharInString.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	fmt.Println(isUniqueCharInString("asdfasdfasdfa"));
+	fmt.Println(isUniqueCharInString("abcdefghijklmnopqrstuvwxyz"));
+}
+
+func isUniqueCharInString(str string) bool {
+	var a [256]bool;
+	for i:=0; i< len(str) ; i++ {
+		if(a[str[i]]) {
+			return false;
+		}
+		a[str[i]] = true;
+	}
+	return true;
+}
